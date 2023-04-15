af6d8e430108c73327499a2c5217d40c1e02886b
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-04-14 23:22:04 -0700

    Render most recent photos

--- src/components/tabs/RecentPhotos.tsx
+++ src/components/tabs/RecentPhotos.tsx
@@ -7,11 +7,15 @@ interface PhotoData {
   filename: string;
   sizeInBytes: number;
   url: string;
+  createdAt: string;
 }
 
 function RecentPhotos() {
+  // Render photos sorted by the most recent
   const { data } = useSelector((state: { photos: { data: PhotoData[] } }) => state.photos);
-  const renderedPhotos = data.map((photo: PhotoData) => (
+  const recentPhotos = data.slice().sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+
+  const renderedPhotos = recentPhotos.map((photo: PhotoData) => (
     <Photo
       key={photo.id}
       imageFileName={photo.filename}
