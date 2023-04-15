15fdf942e62861e023c79a61a4eda58086380513
William Go <gilberthoop@Williams-MacBook-Pro.local>
2023-04-14 17:08:55 -0700

    Code clean up

--- src/components/RecentPhotos.tsx
+++ src/components/RecentPhotos.tsx
@@ -9,18 +9,6 @@ interface PhotoData {
   }
 
 function RecentPhotos() {
-  // const { data } = useSelector((state) => state.photos)
-
-  // const renderedPhotos = data.map((photo) => (
-  //   <Photo
-  //     key={photo.id}
-  //     imageFileName={photo.filename}
-  //     imageFileSize={photo.sizeInBytes}
-  //     imageSource={photo.url}
-  //   />
-  // ));
-
-  // Typescript
   const { data } = useSelector((state: { photos: { data: PhotoData[] } }) => state.photos);
   const renderedPhotos = data.map((photo: PhotoData) => (
     <Photo
